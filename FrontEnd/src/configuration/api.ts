e4a89eb7dcaa66a5bfc5241108329b6aee556d68
YehuditLiba <Y4144246@gmail.com>
2025-06-26 15:38:18 +0300

    upData Dockerfile

--- FrontEnd/src/configuration/api.ts
+++ FrontEnd/src/configuration/api.ts
@@ -3,4 +3,4 @@ const API_MODE = process.env.REACT_APP_API_MODE;
 export const BASE_URL =
     API_MODE === 'prod'
         ? process.env.REACT_APP_API_URL_PROD
-        : process.env.REACT_APP_API_URL_LOCAL ?? 'http://localhost:3000';
+        : process.env.REACT_APP_API_URL_LOCAL ?? 'http://localhost:8080';
